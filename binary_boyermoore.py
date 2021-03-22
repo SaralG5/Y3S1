1d186bafa2a4b83f42a2c2325456c1cc39f34716
SaralG5 <58154468+SaralG5@users.noreply.github.com>
2021-03-23 00:38:42 +1100

    Add files via upload
    fixed more stuff

--- binary_boyermoore.py
+++ binary_boyermoore.py
@@ -1,43 +1,105 @@
-
 # first start off making the rightmost to the left of each position table
-def boyer_moore(txt, pat):
-    # start off with only bad character
-    txt_length = len(txt)
-    pat_length = len(pat)
-    bad_char_array = bad_char_processing(pat)[0]
-    ascii_val = bad_char_processing(pat)[1]
-    full_matches = []
-    counter = 0
-    while counter < txt_length:
-        for_counter = 0
-        # now compare right to left stuff
-        for k in range(pat_length,  0 , -1):
-            asc = ord(txt[k])
-            # if a mismatch occurs we shift stuff
-            if pat[k] != txt[k]:
-                # find rightmost occurrence of letter from position k
-                # Two cases: if the char is in the pat or not
-                if ascii_val[asc] != None:  # this means the txt mismatched character is in the pattern
-                    # check its rightmost occurence
-                    bad_index = ascii_val[asc][0]
-                    occurences = bad_char_array[bad_index]
-                    right_most = biggest_smaller(occurences, k)
-                    # so now we shift by k - right_most position
-                    counter = k - right_most
-                    # and now we start our comparisons from here.
+# Get the z_algorithm from part one, along with reverse string functions.
+
+def z_algorithm(input_str):
+    # first initialise z list and l and r list
+    str_size = len(input_str)
+    if str_size > 1: # if its bigger than one then we will do all the z_stuff
+        z_list = [None for k in range(str_size)]
+        # with the l_list and r_list we will concatenate values to them as we do comparisons.
+        # this is easier since we can easily get the most recent l box and r box values
+        l_list = [str_size]
+        r_list = [str_size]
+        # set first index (0th index) to be the length of the string.
+        z_list[0] = str_size  # set z[0] to be length of string. This is z1.
+        # now we find z2 (which in this case is z_list[1])
+        match_counter = 0  # assign a variable to count the number of matches
+        # now make while loop to count the number of matches.
+        # Ensure that we do not go to end of string and end up getting an index error with the first condition.
+        while 1 + match_counter < str_size and input_str[0 + match_counter] == input_str[1 + match_counter]:
+            match_counter += 1
+        z_list[1] = match_counter  # assign first index to the number of matches. (This is z2 in the lecture slides)
+        if match_counter > 0:  # if number of matches > 0
+            # set r to to be the number of matches.
+            # This is shown as z2 + 1 in lectures cause the lectures 1 index everything.
+            r_list += [match_counter]
+            # set l to 1
+            # (in lectures this is shown to be 2 but I am 0 indexing everything, thought it would be easier that way)
+            l_list += [1]
+        else:
+            # i.e if number of matches is 0 then.
+            # set r to 0
+            r_list += [0]
+            # set l to 0
+            l_list += [0]
+        # now we will do the comparisons in the rest of the string.
+        # make a for loop to go through the rest of the string.
+
+        for k in range(2, str_size):
+            if k > r_list[-1]:  # Case 1 k > r
+                # all we do now is explicitly compare from the start of the string.
+                q_counter = 0  # set a variable to count our number of matches.
+                # I used q cause q was in the lectures.
+                while k + q_counter < str_size and input_str[k + q_counter] == input_str[q_counter]:
+                    # loop through and find matches
+                    q_counter += 1
+                z_list[k] = q_counter
+                if q_counter > 0:  # if zk > 0
+                    # set r to be where last match occurred. In my case this is at k + q_counter - 1 (same as lecture)
+                    r_list += [k + q_counter - 1]
+                    # set l to k value
+                    l_list += [k]
+                else:  # if there were no matches.
+                    # l and r retain there current values
+                    r_list += [r_list[-1]]
+                    l_list += [r_list[-1]]
+
+            else:  # Case 2 where k <= r
+                l = l_list[-1]
+                r = r_list[-1]
+                if z_list[k - l] < r - k + 1:  # case 2a where Z k  - l < r - k
+                    # no need for the +1's as shown in lectures. All lines up fine
+                    # set zk to Z k-l
+                    z_list[k] = z_list[k - l]
+                    # r and l remain unchanged
+                    r_list += [r_list[-1]]
+                    l_list += [l_list[-1]]
+
+
+                elif z_list[k - l] > r - k + 1:  # Case 2b from tute. Zk - l > r - k.
+                    # this case is like when a previous z_box is already bigger your current z_box
+                    # then you will already know that the number of matches is r - k.
+                    z_list[k] = r - k
+                    # nothing happens with l and r so they remain unchanged.
+                    r_list += [r_list[-1]]
+                    l_list += [l_list[-1]]
 
                 else:
-                    # the char in this case is not even in the pat
-                    # so we just shift by 1
-                    counter += 1
-            else:
-                # the character matched
-                for_counter += 1
-                if for_counter == pat_length:   # this means we have a full match
-                    full_matches += [pat_length]
+                    # Case 2c where Z k - l = r - k.
+                    # this is where our two most recent z_boxes are the same length
+                    # Thus we will have to do some explicit comparisons yee yee.
+                    q_counter = 1  # set a variable to be our number of matches
+                    while r + q_counter < str_size and input_str[r + q_counter] == input_str[r - k + 1]:
+                        q_counter += 1
+                    # Set Zk to be  number of matches + the current z_box size.
+                    z_list[k] = (r - l + 1) + q_counter
+                    # with r we update r to be current r value + the number that matches
+                    r_list += [r + q_counter - 1]
+                    # Set l to be k since this is the start of a new z_box that goes outside of our current one.
+                    l_list += [k]
 
+    else:  # if its length 1 then assign all the lists to be the string length.
+        z_list = [1]
+        l_list = [1]
+        r_list = [1]
 
-
+    return z_list
+def reverse_string(string):
+    result = ''
+    n = len(string)
+    for k in range(n - 1, -1 , -1):
+        result += string[k]
+    return result
 
 
 
@@ -54,10 +116,10 @@ def bad_char_processing(a_string):
             ascii_values[asc_value] = (i, a_string[i])
 
         elif ascii_values[asc_value] != None:
-                    most_recent = ascii_values[asc_value]
-                    # this means this string is already in bad_array
-                    # now just add the new index to the list, since we now where the character is
-                    bad_array[most_recent[0]] += [i]
+            most_recent = ascii_values[asc_value]
+            # this means this string is already in bad_array
+            # now just add the new index to the list, since we now where the character is
+            bad_array[most_recent[0]] += [i]
 
         else:  # here the character has not been seen yet
             # so just make a new list for that character
@@ -66,22 +128,19 @@ def bad_char_processing(a_string):
 
     return (bad_array, ascii_values)
 
-print(bad_char_processing('tbapxab'))
-
-
 
 
 def biggest_smaller(a_list, a_value):
     left = 0
     right = len(a_list) - 1
-    if right == left: # this means our list only has one element
+    if right == left:  # this means our list only has one element
         # for this we will just output the list element
         return a_list[0]
 
-    while left <= right:   # while two dividing areas are not the same
+    while left <= right:  # while two dividing areas are not the same
         middle = (left + right) // 2
 
-        if right == left + 1:         # this means we did not find the element
+        if right == left + 1:  # this means we did not find the element
             # now check if the right or left elements are smaller than a_value
             if a_list[right] < a_value:
                 return a_list[right]
@@ -93,13 +152,90 @@ def biggest_smaller(a_list, a_value):
                 # this case is where a_value is in the list but we got to a point where right == left + 1
                 return a_value
 
-        elif a_value > a_list[middle]:   # if middle element is smaller than the one we are looking for
-            left = middle              # set left to be the middle
+        elif a_value > a_list[middle]:  # if middle element is smaller than the one we are looking for
+            left = middle  # set left to be the middle
 
         elif a_value < a_list[middle]:  # if the middle element is bigger than the one we are looking for
-            right = middle              # set right to be the middle
+            right = middle  # set right to be the middle
 
         else:
             # this is the final case where we find the element
             # output the value and index
             return middle, a_value
+
+
+def z_suffix(a_string):
+    # This function returns the z_suffix values for a string
+    # all we got to do is compute the z_values for the string reversed.
+    backward = reverse_string(a_string)
+    return z_algorithm(backward)
+
+
+def good_suffix(a_string):
+    good_suff = []
+    z_suff = z_suffix(a_string)
+    # use function given in lectures
+    m = len(a_string)
+    for j in range(m + 1):
+        good_suff.append(0)
+
+    for p in range(1, m):
+        j = m - z_suff[p]
+        good_suff[j] = p
+    return good_suff
+
+print(good_suffix('abacababaca'))
+
+
+def boyer_moore(txt, pat):
+    result = []
+    # okay so we will first do the part for the bad character rule in the code.
+    comp_counter = 0  # assign variable that will help to compare to all of txt. Stands for comparison counter
+    total_comparisons = 0  # assign a variable to count the total number of comparisons
+    # assign two values to be the lengths of the txt and the pattern
+    txt_length = len(txt)
+    pat_length = len(pat)
+    # now use the array that has the rightmost positions of the string
+    # assign this to two variables
+    bad_char_array = bad_char_processing(pat)[0]  # the rightmost positions
+    already_there = bad_char_processing(pat)[1]  # this contains the ascii values of the letters in pattern
+    # we can use already_there to see if a txt letter we are matching is even in the string
+    while pat_length + comp_counter <= txt_length:  # start outer loop and begin comparisons
+        loop_counter = 1 # this checks if the for loop runs in its entirety.
+        for k in range(pat_length - 1, -1, -1):  # check from left to right for matching characters
+            if pat[k] != txt[k + comp_counter]:  # in this case the characters do not match
+                # now we have to find the rightmost occurrence of the mismatched character in text, within pattern
+                # if we cannot find it then default the shift to 1
+                # first find ascii value of the mismatched character
+                mismatch_asc = ord(txt[k])
+                if already_there[mismatch_asc] is not None:  # here the mismatched character is in text
+                    # now find the rightmost occurrence of this character
+                    index_and_value = already_there[mismatch_asc]  # assign variable to hold the index and value
+                    # of the mismatched character
+                    index_in_pat = index_and_value[0]  # assign variable to index of mismatched character in pat
+                    # the bad character list has the occurrences of this letter.
+                    # to find them:
+                    right_most = biggest_smaller(bad_char_array[index_in_pat], k)
+                    # so now we got the right most occurrence of the mismatched character yee yee.
+                    # Now we will assign a variable to the amount we got to shift by
+                    shift_maybe = k - right_most
+                    # the actual shift will be the maximum of this value and 1. Thus:
+                    actual_shift = max(1, shift_maybe)
+                    comp_counter += actual_shift  # shift the pattern forward.
+                    break  # now break out of the for loop no more comparison needed.
+
+                else:  # here the mismatched character ain't even in text man.
+                    actual_shift = 1  # so we just shift 1 ahead.
+                    comp_counter += actual_shift
+
+            else:  # in this case the characters actually match so all good
+                if loop_counter == pat_length:  # if the whole pattern matched
+                    # then we can shift the pattern forward by pat_length
+                    result += [k + comp_counter]  # put the index where full match ends into the result list
+                    comp_counter += pat_length # maybe have to get rid of this.
+                else:  # otherwise keep checking
+                    loop_counter += 1
+    return result
+
+print(boyer_moore('0011010101111001001101100', '010'))
+
--- editdist.py
+++ editdist.py
@@ -131,3 +131,102 @@ print(reverse_string('abcd'))
 print(dist_finder('abdyabxdcyabcdz', 'abcd'))
 print(dist_finder('zxycade','cab'))
 #print(z_algorithm('aaba'))
+
+
+# one below is real one
+def z_algorithm_two(input_str):
+    # first initialise z list and l and r list
+    str_size = len(input_str)
+    if str_size > 1: # if its bigger than one then we will do all the z_stuff
+        z_list = [None for k in range(str_size)]
+        # with the l_list and r_list we will concatenate values to them as we do comparisons.
+        # this is easier since we can easily get the most recent l box and r box values
+        l_list = [str_size]
+        r_list = [str_size]
+        # set first index (0th index) to be the length of the string.
+        z_list[0] = str_size  # set z[0] to be length of string. This is z1.
+        # now we find z2 (which in this case is z_list[1])
+        match_counter = 0  # assign a variable to count the number of matches
+        # now make while loop to count the number of matches.
+        # Ensure that we do not go to end of string and end up getting an index error with the first condition.
+        while 1 + match_counter < str_size and input_str[0 + match_counter] == input_str[1 + match_counter]:
+            match_counter += 1
+        z_list[1] = match_counter  # assign first index to the number of matches. (This is z2 in the lecture slides)
+        if match_counter > 0:  # if number of matches > 0
+            # set r to to be the number of matches.
+            # This is shown as z2 + 1 in lectures cause the lectures 1 index everything.
+            r_list += [match_counter]
+            # set l to 1
+            # (in lectures this is shown to be 2 but I am 0 indexing everything, thought it would be easier that way)
+            l_list += [1]
+        else:
+            # i.e if number of matches is 0 then.
+            # set r to 0
+            r_list += [0]
+            # set l to 0
+            l_list += [0]
+        # now we will do the comparisons in the rest of the string.
+        # make a for loop to go through the rest of the string.
+
+        for k in range(2, str_size):
+            if k > r_list[-1]:  # Case 1 k > r
+                # all we do now is explicitly compare from the start of the string.
+                q_counter = 0  # set a variable to count our number of matches.
+                # I used q cause q was in the lectures.
+                while k + q_counter < str_size and input_str[k + q_counter] == input_str[q_counter]:
+                    # loop through and find matches
+                    q_counter += 1
+                z_list[k] = q_counter
+                if q_counter > 0:  # if zk > 0
+                    # set r to be where last match occurred. In my case this is at k + q_counter - 1 (same as lecture)
+                    r_list += [k + q_counter - 1]
+                    # set l to k value
+                    l_list += [k]
+                else:  # if there were no matches.
+                    # l and r retain there current values
+                    r_list += [r_list[-1]]
+                    l_list += [r_list[-1]]
+
+            else:  # Case 2 where k <= r
+                l = l_list[-1]
+                r = r_list[-1]
+                if z_list[k - l] < r - k + 1:  # case 2a where Z k  - l < r - k
+                    # no need for the +1's as shown in lectures. All lines up fine
+                    # set zk to Z k-l
+                    z_list[k] = z_list[k - l]
+                    # r and l remain unchanged
+                    r_list += [r_list[-1]]
+                    l_list += [l_list[-1]]
+
+
+                elif z_list[k - l] > r - k + 1:  # Case 2b from tute. Zk - l > r - k.
+                    # this case is like when a previous z_box is already bigger your current z_box
+                    # then you will already know that the number of matches is r - k.
+                    z_list[k] = r - k
+                    # nothing happens with l and r so they remain unchanged.
+                    r_list += [r_list[-1]]
+                    l_list += [l_list[-1]]
+
+                else:
+                    # Case 2c where Z k - l = r - k.
+                    # this is where our two most recent z_boxes are the same length
+                    # Thus we will have to do some explicit comparisons yee yee.
+                    q_counter = 1  # set a variable to be our number of matches
+                    while r + q_counter < str_size and input_str[r + q_counter] == input_str[r - k + 1]:
+                        q_counter += 1
+                    # Set Zk to be r - k + q_counter. This is the number of matches + the current z_box size.
+                    z_list[k] = (r - l + 1) + q_counter
+                    # with r we update r to be current r value + the number that matches
+                    r_list += [r + q_counter - 1]
+                    # Set l to be k since this is the start of a new z_box that goes outside of our current one.
+                    l_list += [k]
+
+    else:  # if its length 1 then assign all the lists to be the string length.
+        z_list = [1]
+        l_list = [1]
+        r_list = [1]
+
+    return z_list
+
+print(z_algorithm_two('abacababaca'))
+
