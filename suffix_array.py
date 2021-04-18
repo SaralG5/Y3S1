143bea726157a43ef63a05f29bcc5b0bfe7690bd
SaralG5 <58154468+SaralG5@users.noreply.github.com>
2021-04-18 20:58:52 +1000

    Add files via upload
    Cant do rule 2. I think there is a problem when there are two suffixes on the same branch

--- suffix_array.py
+++ suffix_array.py
@@ -81,11 +81,14 @@ class SuffixTree:
 
                 else:  # if the root node does have edges
                     no_edges = len(self.root.edges)
+                    rule_two_root = True
+                  
                     # then check if the current string is a prefix of any suffixes in the root edge list
                     for i in range(no_edges):
+
                         edge_string = self.root.edges[i][0]
                         rule_one_check = is_prefix(edge_string, current_suffix)
-                        rule_three_check = is_prefix(current_suffix, edge_string)
+                        rule_two_check = is_prefix(current_suffix, edge_string)
                         if rule_one_check[0]:
                             # since we are building the tree we check if
                             # the current edges are prefixes of the string at a given suffix extension for rule 1
@@ -93,34 +96,47 @@ class SuffixTree:
                             # Since we are at a 'leaf'
                             edge_string += current_suffix[-1]
                             self.root.edges[i][0] = edge_string  # add the last character of string to edge.
-                        elif rule_three_check[0]:  # Rule 3
-                            pass  # do nothing
-                        else:  # Rule 2 Extension
-                            print(current_suffix, edge_string)
-                            # we can either have a node at the point of mismatch or not
+                            rule_two_root = False
+                            break
+                        elif rule_two_check[0]:  # Rule 3
+                            rule_two_root = False
+                            break  # do nothing
+                        else:
+                            # Rule 2 Extension
+
                             mismatch_index = rule_two_check[1]
-                            mismatched_char = current_suffix[mismatch_index]
-                            if mismatch_index == 0:  # alternative case for root, means first letter not match
-                                self.root.edges.append([current_suffix, False])
-                            else:
+                            if mismatch_index >= 1:
+                                # # we can either have a node at the point of mismatch or not
+
+                                mismatched_char = part_string(mismatch_index, len(current_suffix) -1, current_suffix)
+                                print(mismatch_index, mismatched_char)
                                 if type(self.root.edges[i][1]) == TreeNode:  # alternative case not root Rule 2
+
                                     already_node = self.root.edges[i][1]
+
                                     # check if this node has any edges
                                     if not already_node.has_edges:  # if there are no edges
+
                                         # then add the mismatched character
                                         already_node.edges.append([mismatched_char, False])
                                         already_node.has_edges = True
                                         self.root.edges[i][1] = already_node
                                     else:  # if there are edges
                                         # then we have to check which one to add to
+                                        
                                         amount_char = len(already_node.edges)
                                         added = False
                                         for j in range(amount_char):
-                                            if is_prefix(mismatched_char, already_node.edges[j][0]):
-                                                already_node.edges[j][0] += mismatched_char
+                                            already_string = already_node.edges[j][0]
+                                            if is_prefix(already_string, mismatched_char)[0]:
+
+                                                already_string += mismatched_char[-1]
+                                                already_node.edges[j][0] = already_string
                                                 added = True
                                                 self.root.edges[i][1] = already_node
+
                                         if not added:  # means mismatched char is not in nodes edges
+
                                             already_node.edges.append([mismatched_char, False])
                                             self.root.edges[i][1] = already_node
 
@@ -128,7 +144,12 @@ class SuffixTree:
                                     # add a node
                                     new_node = TreeNode()
                                     new_node.edges.append([mismatched_char, False])
+                                    new_node.has_edges = True
                                     self.root.edges[i][1] = new_node
+                                rule_two_root = False
+                                break  # break out the for loop
+                    if rule_two_root:
+                        self.root.edges.append([current_suffix, False])
 
 
 
@@ -147,6 +168,9 @@ class SuffixTree:
 
 
 
-a = SuffixTree('abac')
+
+
+a = SuffixTree('abacabad')
 a.ukkonen()
 print(a.root.edges)
+print(a.root.edges[0][1].edges)
