9a147f8b5612a5b539273f5f2e2b9c4b2659501b
SaralG5 <58154468+SaralG5@users.noreply.github.com>
2021-04-22 17:39:00 +1000

    Add files via upload
    Have to implement suffix arrays still

--- suffix_array.py
+++ suffix_array.py
@@ -10,31 +10,30 @@ def is_prefix(a_string, first_tup, second_tup):
     """
 
     size_first_tup = abs(first_tup[0] - first_tup[1]) + 1  # create variable representing size of string for first_tup
-    #size_second_tup = second_tup[0] + second_tup[1] + 1  # create variable for size of string representing second_tup.
+    # size_second_tup = second_tup[0] + second_tup[1] + 1  # create variable for size of string representing second_tup.
     start_first = first_tup[0]  # get the starting index of the first tuple.
     start_second = second_tup[0]  # get the starting index of the second tuple.
     counter = 0
-    #if first_tup <= second_tup:
+    # if first_tup <= second_tup:
     while counter < size_first_tup:
         if a_string[start_first + counter] != a_string[start_second + counter]:
             return False, start_first + counter
         counter += 1
     return True, start_second + counter
 
-
 class SuffixTree:
     def __init__(self, a_string):
         self.input_string = a_string  # the input string
-        self.root_list = []   # this list will act as the root node for the suffix tree
+        self.root_list = []  # this list will act as the root node for the suffix tree
         for k in range(122):  # 122 because the assignment specs say that 122 is the highest ascii value
             self.root_list.append([])  # append an empty list
             # each empty list will act as a branch from the root node
 
     def ukkonen(self):
         global_end = 0  # initialise global end.
-        last_j = 0      # initialise last_j which is the last place that you did a rule 2/1.
-        phase = 0      # initialise phase.
-        extension = 0   # initialise extension.
+        last_j = 0  # initialise last_j which is the last place that you did a rule 2/1.
+        phase = 0  # initialise phase.
+        extension = 0  # initialise extension.
         size_string = len(self.input_string)  # the length of the input string.
         while phase < size_string:  # while the phases are less than the string length.
             extension = 0
@@ -46,52 +45,75 @@ class SuffixTree:
                 # now find the ascii value of the first character in current string.
                 asc_value = ord(self.input_string[extension])
                 # Now check if the root list has anything at this ascii value
-                if not self.root_list[asc_value]: # if there is nothing there
-                    self.root_list[asc_value].append(extension) # append the index of the current letter
+                if not self.root_list[asc_value]:  # if there is nothing there
+                    self.root_list[asc_value].append(extension)  # append the index of the current letter
                     extension += 1
                 else:  # Otherwise if we have an edge.
                     # (i.e there is an integer in the list there at the index of the ascii value)
                     # Then we have to see if we have a rule 1, 2 or 3 situation
-                    extension = last_j + 1 # In general the comparisons will start from last_j onwards
+                    extension = last_j + 1  # In general the comparisons will start from last_j onwards
                     asc_value = ord(self.input_string[extension])
                     if not self.root_list[asc_value]:  # if there is nothing there
                         self.root_list[asc_value].append(extension)  # append the index of the current letter
                         # increment global_end, extension and last_j
                         extension += 1
                         last_j += 1
-                    else: # if there is an edge
+                    else:  # if there is an edge
                         current_string = [extension, phase]  # get current string of extension and phase
-                        edge_string = [self.root_list[asc_value][0], global_end] # get string on edge
+                        edge_string = [self.root_list[asc_value][0], global_end]  # get string on edge
                         # check if current string is a prefix of whats on the edge
                         is_a_prefix = is_prefix(self.input_string, current_string, edge_string)
                         # if it is a prefix then we have a rule 3 situation
-                        if is_a_prefix[0]:   # if it is a prefix
+                        if is_a_prefix[0]:  # if it is a prefix
                             # then we can stop this phase
                             break
-                        else: # if not then it will be a rule 2 situation
+                        else:  # if not then it will be a rule 2 situation
                             # in rule 2's we have to deal with suffix links and all that jazz
-                            # first find which letter did not match
-                            mismatch_index = is_a_prefix[1]
-                            # Now create a new node a in the root list for current letter that contains the current
-                            # string. Append a tuple that represents this node in spirit
-                            # now we have to go to every other string and add a node for it
                             for i in range(extension, global_end + 1):
                                 # go to each index of the starting letter
                                 index_letter = ord(self.input_string[i])
-                                    # add to the root list
+                                # add to the root list
                                 self.root_list[index_letter].append(i)
                             last_j = global_end  # reset last_j value
-                            break   # can go straight to the next phase after this
+                            break  # can go straight to the next phase after this
             global_end += 1
             phase += 1
-        return self.root_list
+        # now iterate and make everything output friendly, Still O(n) yee yee
+        output_list = []
+        ranks = []
+        different_lets = 0
+        for k in self.root_list:
+            if k:
+                for i in k:
+                    ranks.append(different_lets)
+                    output_list.append(i)
+                different_lets += 1
+        return output_list
+
+    def suffix_array(self, id_list, rank_list):
+        k = 1
+        jumps = 2 ** k
+        size_lists = len(id_list)
+        temp = [0 for i in range(size_lists)]
+        counter = 0
+        while counter < size_lists - 1:
+            # compare each thing
+            if rank_list[counter] < rank_list[counter + 1]:
+                temp[id_list[counter + 1]] = rank_list[counter] + 1
+            else:
+                check_one = id_list[counter] + jumps
+                check_two = id_list[counter + 1] + jumps
+                if check_one >= size_lists:
+                    check_one = size_lists - 1
+                elif check_two >= size_lists:
+                    check_two = size_lists - 1
+
+
+
+
 
 
 a = SuffixTree('mississippi$')
 print(a.ukkonen())
 
-
-
-
-            # extension  = 0
-
+# extension  = 0
