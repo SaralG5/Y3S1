eb4268052b1dbc080af966cd354eac743a221dd5
SaralG5 <58154468+SaralG5@users.noreply.github.com>
2021-04-29 17:10:24 +1000

    Add files via upload
    Need to do q3 now

--- suffix_array.py
+++ suffix_array.py
@@ -53,35 +53,25 @@ class SuffixTree:
                     # (i.e there is an integer in the list there at the index of the ascii value)
                     # Then we have to see if we have a rule 1, 2 or 3 situation
                     extension = last_j + 1  # In general the comparisons will start from last_j onwards
-                    asc_value = ord(self.input_string[extension])
-                    if not self.root_list[asc_value]:  # if there is nothing there
-                        self.root_list[asc_value].append(extension)  # append the index of the current letter
-                        # increment global_end, extension and last_j
-                        extension += 1
-                        last_j += 1
-                    else:  # if there is an edge
-                        current_string = [extension, phase]  # get current string of extension and phase
-                        edge_string = [self.root_list[asc_value][0], global_end]  # get string on edge
-                        # check if current string is a prefix of whats on the edge
-                        is_a_prefix = is_prefix(self.input_string, current_string, edge_string)
-                        # if it is a prefix then we have a rule 3 situation
-                        if is_a_prefix[0]:  # if it is a prefix
-                            # then we can stop this phase
-                            break
-                        else:  # if not then it will be a rule 2 situation
-                            # in rule 2's we have to deal with suffix links and all that jazz
-                            for i in range(extension, global_end + 1):
-                                # go to each index of the starting letter
-                                index_letter = ord(self.input_string[i])
-                                # add to the root list
-                                self.root_list[index_letter].append(i)
-                            last_j = global_end  # reset last_j value
-                            break  # can go straight to the next phase after this
+                    current_string = [extension, phase]  # get current string of extension and phase
+                    edge_string = [self.root_list[asc_value][0], global_end]  # get string on edge
+                    # check if current string is a prefix of whats on the edge
+                    is_a_prefix = is_prefix(self.input_string, current_string, edge_string)
+                    # if it is a prefix then we have a rule 3 situation
+                    if is_a_prefix[0]:  # if it is a prefix
+                        # then we can stop this phase
+                        break
+                    else:  # if not then it will be a rule 2 situation
+                        # in rule 2's we have to deal with suffix links and all that jazz
+                        for i in range(extension, global_end + 1):
+                            # go to each index of the starting letter
+                            index_letter = ord(self.input_string[i])
+                            # add to the root list
+                            self.root_list[index_letter].append(i)
+                        last_j = global_end  # reset last_j value
+                        break  # can go straight to the next phase after this
             global_end += 1
             phase += 1
-        # So at this point we have all the information about the suffixes in the root list
-        # To construct the suffix array we need an array indexed by id that contains the rank of each suffix
-        # so we will construct this and then pass this into the suffix array function.
         return global_end - 1, self.root_list
 
 
@@ -179,7 +169,7 @@ def ukkonen_full(a_string):
             suffix_array += binary.retrieve()
     return [end + 1] + suffix_array
 
-print(ukkonen_full('mississippi'))
+print(ukkonen_full('abcdabcbdab'))
 #a = SuffixTree('mississippi')
 #print(a.ukkonen())
 # b = BinaryTree('mississippi', 10)
@@ -188,3 +178,59 @@ print(ukkonen_full('mississippi'))
 # b.insert(7)
 # b.insert(10)
 # print(b.retrieve())
+
+#
+# def ukkonen_other(self):
+#     global_end = 0  # initialise global end.
+#     last_j = 0  # initialise last_j which is the last place that you did a rule 2/1.
+#     phase = 0  # initialise phase.
+#     extension = 0  # initialise extension.
+#     size_string = len(self.input_string)  # the length of the input string.
+#     while phase < size_string:  # while the phases are less than the string length.
+#         extension = 0
+#         while extension <= phase:  # while the extensions are less then the phases.
+#             # the string we have to compare to root node branches is: str[extension: phase].
+#             # this can be represented as two integers [extension,phase].
+#             # So firstly we have to check if the root node list even has stuff in it.
+#             current_string = [extension, phase]  # get the current string for that extension and phase.
+#             # now find the ascii value of the first character in current string.
+#             asc_value = ord(self.input_string[extension])
+#             # Now check if the root list has anything at this ascii value
+#             if not self.root_list[asc_value]:  # if there is nothing there
+#                 self.root_list[asc_value].append(extension)  # append the index of the current letter
+#                 extension += 1
+#             else:  # Otherwise if we have an edge.
+#                 # (i.e there is an integer in the list there at the index of the ascii value)
+#                 # Then we have to see if we have a rule 1, 2 or 3 situation
+#                 extension = last_j + 1  # In general the comparisons will start from last_j onwards
+#                 asc_value = ord(self.input_string[extension])
+#                 if not self.root_list[asc_value]:  # if there is nothing there
+#                     print('here')
+#                     self.root_list[asc_value].append(extension)  # append the index of the current letter
+#                     # increment global_end, extension and last_j
+#                     extension += 1
+#                     last_j += 1
+#                 else:  # if there is an edge
+#                     current_string = [extension, phase]  # get current string of extension and phase
+#                     edge_string = [self.root_list[asc_value][0], global_end]  # get string on edge
+#                     # check if current string is a prefix of whats on the edge
+#                     is_a_prefix = is_prefix(self.input_string, current_string, edge_string)
+#                     # if it is a prefix then we have a rule 3 situation
+#                     if is_a_prefix[0]:  # if it is a prefix
+#                         # then we can stop this phase
+#                         break
+#                     else:  # if not then it will be a rule 2 situation
+#                         # in rule 2's we have to deal with suffix links and all that jazz
+#                         for i in range(extension, global_end + 1):
+#                             # go to each index of the starting letter
+#                             index_letter = ord(self.input_string[i])
+#                             # add to the root list
+#                             self.root_list[index_letter].append(i)
+#                         last_j = global_end  # reset last_j value
+#                         break  # can go straight to the next phase after this
+#         global_end += 1
+#         phase += 1
+#     # So at this point we have all the information about the suffixes in the root list
+#     # To construct the suffix array we need an array indexed by id that contains the rank of each suffix
+#     # so we will construct this and then pass this into the suffix array function.
+#     return global_end - 1, self.root_list
